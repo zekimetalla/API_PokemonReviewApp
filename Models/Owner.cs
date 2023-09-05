11fd2d19c60da1c0662645498be9a9f06b367925
zekimetalla <144137167+zekimetalla@users.noreply.github.com>
2023-09-05 16:36:37 +0200

    Add files via upload

---
+++ Models/Owner.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Owner
+    {
+        internal string FirstName;
+        internal string LastName;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+        public Country Country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+    }
+}
